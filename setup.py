62b76ea9a4261703708bde83c56524812de4b620
Ilya Shamovsky <ilya.shamovsky@gmail.com>
2017-07-10 20:44:21 -0400

    Added example fastq file.

--- setup.py
+++ setup.py
@@ -27,26 +27,23 @@ def get_package_data():
         ]}
 
 setup(
-    name="pyhop",
-    version=extract_version(),
-    author="Aviram Rasouly, Ilya Shamovsky",
-    author_email="ilya.shamovsky@nyumc.org",
-    url="https://github.com/eco32i/pyhop",
-    license="MIT",
-    packages=find_packages(),
-    package_dir={"pyhop": "pyhop"},
-    package_data=get_package_data(),
-    description="TN-Seq anaylsis pipeline",
+    name = "pyhop",
+    version = extract_version(),
+    author = "Aviram Rasouly, Ilya Shamovsky",
+    author_email = "ilya.shamovsky@nyumc.org",
+    url = "https://github.com/eco32i/pyhop",
+    license = "MIT",
+    packages = find_packages(),
+    package_dir = {"pyhop": "pyhop"},
+    package_data = get_package_data(),
+    description = "TN-Seq anaylsis pipeline",
     # run pandoc --from=markdown --to=rst --output=README.rst README.md
-    long_description=open("README.md").read(),
+    long_description = open("README.md").read(),
     # numpy is here to make installing easier... Needs to be at the
     # last position, as that's the first installed with
     # "python setup.py install"
-    install_requires=["numpy",
-                      "pandas"]
-    entry_points={
-        'console_scripts': [
-# This is temporary
+    install_requires = ["numpy", "pandas"],
+    entry_points = {'console_scripts': [
             'pyhop=pyhop.dmux:main',
             ],
         },
